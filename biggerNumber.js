8e80dede5107cf26f417069aacb065a1be981abe
aHasnat1997 <hasnat56703@gmail.com>
2023-02-05 21:21:47 +0600

    File Name Update

---
+++ biggerNumber.js
@@ -0,0 +1,13 @@
+var num = [50, 6];
+
+
+if(num[0] < num[1]){
+  console.log(num[1]+ ' ' +'is Bigger than'+ ' ' +num[0]);
+}
+else{
+  console.log(num[0]+ ' ' +'is Bigger than'+ ' ' +num[1]);
+}
+
+// num.sort();
+
+console.log(num);
